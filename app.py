fdc411ff8cd1d2570ac2c393ff639d5cf11c6d90
gilkarina89-png <gilkarina89@gmail.com>
2026-05-28 22:56:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from aiohttp import web
 import asyncio
 import logging
-import os
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
@@ -13,7 +11,6 @@ import handlers
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    asyncio.create_task(start_health_server())
     # Инициализация базы данных
     init_db()
     
@@ -27,18 +24,6 @@ async def main():
     # Запуск бота
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-# ========= ВЕБ-СЕРВЕР ДЛЯ RENDER =========
-async def health_check(request):
-    return web.Response(text="OK")
 
-async def start_health_server():
-    app = web.Application()
-    app.router.add_get("/", health_check)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    port = int(os.environ.get("PORT", 10000))
-    site = web.TCPSite(runner, "0.0.0.0", port)
-    await site.start()
-    print(f"Health check server started on port {port}")
 if __name__ == "__main__":
     asyncio.run(main())
