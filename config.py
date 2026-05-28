5405d9001abaa7d7548a5982a53f77b395177bd6
gilkarina89-png <gilkarina89@gmail.com>
2026-05-28 20:24:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "8926072756:AAFr0H_cqW_djnXLt7nlncMPVaaiW1UeOPY"
+BOT_TOKEN = "8926072756:AAHqZP3ByWKcSHW7XCtojblGrJ0hYgjCE8Q"
 
 # Здесь можно хранить ссылки для внешних источников
 KINOPOISK_BASE_URL = "https://www.kinopoisk.ru/search/?q="
